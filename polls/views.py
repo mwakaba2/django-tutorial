43f0d1ba777f65c3f35ac21a0bb17f753a43d723
Mariko Wakabayashi <mwakabayashi@groupon.com>
2017-08-17 18:10:01 -0500

    complete part 3

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
 from django.template import loader
@@ -16,7 +16,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
